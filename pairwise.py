e47b6e22c0af487b01d2bb1da68699e044053337
Mustansar Fiaz <mustansar3797@gmail.com>
2020-08-31 00:13:32 -0700

    Update pairwise.py

--- pairwise.py
+++ pairwise.py
@@ -95,26 +95,7 @@ class Pairwise(Dataset):
     def __len__(self):
         return self.cfg.pairs_per_seq * len(self.seq_dataset)
 
-    def sp_noise(self, image, prob):
-        '''
-        Add salt and pepper noise to image
-        prob: Probability of the noise
-        '''
-        image = np.array(image)
-        output = np.zeros(image.shape,np.uint8)
-        thres = 1 - prob
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                rdn = random.random()
-                if rdn < prob:
-                    output[i][j] = 0
-                elif rdn > thres:
-                    output[i][j] = 255
-                else:
-                    output[i][j] = image[i][j]
-        cv2.imwrite("cv.png", output)
-        output = Image.fromarray(output)
-        return output
+    
     def softMask(self, image, box):
         '''
         Add softMask
